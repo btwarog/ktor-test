50fccd47d4a4791c5cce8db45743d279ac8500d5
Bartłomiej Twaróg <b.twarog90@gmail.com>
2023-09-23 23:29:36 +0200

    Added basic server setup

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+        client.get("/bartek").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello Bartek!", bodyAsText())
+        }
+    }
+}
