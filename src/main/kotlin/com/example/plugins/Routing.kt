20e97b3a58fd6a2ede954feb012b42fa3457c516
Bartłomiej Twaróg <b.twarog90@gmail.com>
2023-09-24 12:49:38 +0200

    Added 'customers' route

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,39 @@
 package com.example.plugins
 
+import com.example.models.customerStorage
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 
 
 fun Application.configureRouting() {
     routing {
+        route("/customers") {
+            get {
+                if (customerStorage.isNotEmpty()) {
+                    call.respond(customerStorage)
+                } else {
+                    call.respondText("No customers found", status = HttpStatusCode.OK)
+                }
+            }
+        }
+        route("/customer") {
+            get("{id?}") {
+
+            }
+            post {
+
+            }
+            delete("{id?}") {
+
+            }
+        }
     }
 }
